2b16bae8e96c65f63901832bd7b6b850e2f59599
Michael Odintsov <michael@datarobot.com>
2015-08-11 09:10:23 +0300

    Add registration form

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,30 +1,33 @@
-
 from django import forms
 from django.contrib import admin
 from ckeditor.widgets import CKEditorWidget
 
-from blog.models import Category, Post
+from blog.models import Category, Post, UserProfile
 
 # Register your models here.
 
 def make_published(modeladmin, request, queryset):
     queryset.update(status='1')
+
+
 make_published.short_description = "Mark selected stories as published"
+
 
 class PostAdminForm(forms.ModelForm):
     body = forms.CharField(widget=CKEditorWidget())
 
     class Meta:
         model = Post
-        fields = "__all__" 
+        fields = "__all__"
+
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('title', 'category', 'status','updated')
+    list_display = ('title', 'category', 'status', 'updated')
     search_fields = ['title']
     date_hierarchy = 'updated'
     ordering = ['title']
-    actions = [make_published, 'make_draft','make_unpublished']
+    actions = [make_published, 'make_draft', 'make_unpublished']
     form = PostAdminForm
 
     def make_unpublished(self, request, queryset):
@@ -34,18 +37,20 @@ class PostAdmin(admin.ModelAdmin):
         else:
             message_bit = "%s stories were" % rows_updated
         self.message_user(request, "%s successfully marked as unpublished." % message_bit)
+
     make_unpublished.short_description = "Mark selected stories as unpublished"
-    
+
     def make_draft(self, request, queryset):
         queryset.update(status='0')
+
     make_draft.short_description = "Mark selected stories as draft"
+
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
+# admin.site.register(Post, PostAdmin)
 
-
-#admin.site.register(Post, PostAdmin)
-
-admin.site.register(Category,CategoryAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
+admin.site.register(UserProfile)
--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,28 +1,32 @@
 # -*- coding:utf-8 -*-
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
-import datetime 
+import datetime
 from blog.models import Category, Post
 from django.views.generic import TemplateView
+from blog.forms import UserForm, UserProfileForm
 
 # Create your views here.
 #  home_page = None
-class MyStruct(object): 
-	pass 
+class MyStruct(object):
+    pass
+
 
 def home_page(request):
-    return HttpResponse('<html><title>Blog lists</title>Hello Blog <br> "You must be joking!" I can hear you say.</html>')
+    return HttpResponse(
+        '<html><title>Blog lists</title>Hello Blog <br> "You must be joking!" I can hear you say.</html>')
+
 
 def index(request):
-	posts_list = Post.objects.order_by('title')
-	
-	category_list = Category.objects.order_by('name')
-	context_dict = {'categories_list':category_list, 'posts_list':posts_list }
-	return render(request, 'blog/index.html',  context_dict)
+    posts_list = Post.objects.order_by('title')
+
+    category_list = Category.objects.order_by('name')
+    context_dict = {'categories_list': category_list, 'posts_list': posts_list}
+    return render(request, 'blog/index.html', context_dict)
+
 
 def view(request, postslug):
     post = Post.objects.get(slug=postslug)
-
     context = {'post': post}
     return render_to_response('blog/singlepost.html', context)
 
@@ -30,14 +34,66 @@ def view(request, postslug):
 class AboutView(TemplateView):
     template_name = "about.html"
 
+
 def categories(request):
-	category_list = Category.objects.order_by('name')
-			
-	context_dict = {'category_list':category_list }
-	return render(request, 'blog/cat.html', context_dict)
+    category_list = Category.objects.order_by('name')
+
+    context_dict = {'category_list': category_list}
+    return render(request, 'blog/cat.html', context_dict)
+
 
 def category(request, categoryslug):
-	name = Category.objects.get(slug=categoryslug)
-	posts = Post.objects.filter(category=name)
-	context = {'posts': posts}
-	return render(request, 'blog/singlecategory.html', context)
+    name = Category.objects.get(slug=categoryslug)
+    posts = Post.objects.filter(category=name)
+    context = {'posts': posts}
+    return render(request, 'blog/singlecategory.html', context)
+
+
+def register(request):
+    # boolean value
+    # Установлено в False при инициализации.
+    # Изменим на True при успешной регистрации.
+
+    registered = False
+
+    # Если HTTP POST, обработаем форму.
+    if request.method == 'POST':
+        # Получаем информацию из форм.
+        # Мы используем две формы UserForm и UserProfileForm.
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+        # Если обе формы прошли проверку...
+        if user_form.is_valid() and profile_form.is_valid():
+            # Сохраним данные пользователя из формы в database.
+            user = user_form.save()
+
+            # Хешируем пароль с помощью set_password method.
+
+            user.set_password(user.password)
+            user.save()
+            # Пока пользователь настраивает свой профиль не выполнять commit=False.
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            # Юзер хочет картинку?
+            # Если да, предоставим ему поле для ввода картинки.
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            # Сохранить экземпляр модели UserProfile.
+            profile.save()
+
+            # Изменить переменную при успешной регистрации.
+            registered = True
+        else:
+            print user_form.errors, profile_form.errors
+            # Не HTTP POST, строим два экземпляра ModelForm .
+    # Эти формы пустые , предназначены для пользовательских вводов.
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+
+    # Render the template depending on the context.
+    return render(request, 'blog/register.html',
+                  {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
